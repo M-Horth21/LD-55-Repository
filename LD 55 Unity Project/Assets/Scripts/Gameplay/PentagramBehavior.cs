96864100e5e78658d2f8b281badf08e03bb9368d
Mateuuu <matthewal194@gmail.com>
2024-04-13 00:00:38 -0700

    World Building

---
+++ LD 55 Unity Project/Assets/Scripts/Gameplay/PentagramBehavior.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System;
+
+public class PentagramBehavior : MonoBehaviour
+{
+    [SerializeField] string sceneName;
+
+    [SerializeField]
+    InputActionReference _clickAction;
+
+    [SerializeField] GameObject glow;
+
+    private bool clicked = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        glow.SetActive(true);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (!clicked && _clickAction.action.ReadValue<float>() > 0)
+        {
+            clicked = true;
+            Debug.Log("test");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        glow.SetActive(false);
+    }
+}
