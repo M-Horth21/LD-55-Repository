c5ef0d31175e766da1feb66025bbc272de7f1d64
Mike Horth <mhorth@me.com>
2024-04-13 14:14:30 -0400

    Pentagrams have UI canvases for "press to enter".

--- LD 55 Unity Project/Assets/Scripts/Gameplay/PentagramBehavior.cs
+++ LD 55 Unity Project/Assets/Scripts/Gameplay/PentagramBehavior.cs
@@ -4,9 +4,17 @@ using UnityEngine.SceneManagement;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using System;
+using TMPro;
 
 public class PentagramBehavior : MonoBehaviour
 {
+    enum Difficulty
+    {
+        Easy,
+        Medium,
+        Hard
+    }
+
     [Scene]
     [SerializeField] string sceneName;
 
@@ -15,15 +23,33 @@ public class PentagramBehavior : MonoBehaviour
 
     [SerializeField] GameObject glow;
 
+    [SerializeField]
+    TextMeshProUGUI _tipText;
+
+    [SerializeField]
+    GameObject _tipCanvas;
+
+    [SerializeField]
+    Difficulty _difficulty = Difficulty.Easy;
+
+    private void Awake()
+    {
+        glow.SetActive(false);
+        _tipCanvas.SetActive(false);
+        _tipText.text = $"Press space to enter {_difficulty} level";
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         glow.SetActive(true);
+        _tipCanvas.gameObject.SetActive(true);
         _clickAction.action.performed += HandleEnter;
     }
 
     private void OnTriggerExit(Collider other)
     {
         glow.SetActive(false);
+        _tipCanvas.SetActive(false);
         _clickAction.action.performed -= HandleEnter;
     }
 
