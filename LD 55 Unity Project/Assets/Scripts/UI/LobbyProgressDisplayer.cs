58f3a646b924aad4e0373ec3c380387c74002e73
Mike Horth <mhorth@me.com>
2024-04-14 10:58:48 -0400

    Show portals completed progress in lobby.

---
+++ LD 55 Unity Project/Assets/Scripts/UI/LobbyProgressDisplayer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LobbyProgressDisplayer : MonoBehaviour
+{
+    [SerializeField]
+    GameState _gameState;
+
+    TextMeshProUGUI _text;
+
+    void Awake()
+    {
+        _text = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _text.text = $"{_gameState.NumberOfCompletedPortals}/11 portals completed";
+    }
+}
